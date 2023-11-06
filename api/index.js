4ea0a75b6de22d53425ed3d440e0c1f2097931a8
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-11-06 06:21:51 +0500

    realm-web

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import * as Realm from "realm-web";
 
-const app = new Realm.App({id: "APP_ID"});
-const mongo = app.currentUser.mongoClient("DATA_SOURCE_NAME");
-const collection = mongo.db("DATABASE_NAME").collection("COLLECTION_NAME");
+export default async () => {
 
-export default () => Response.json(collection);
+    const app = new Realm.App({id: "APP_ID"});
+    const credentials = Realm.Credentials.anonymous();
+    const user = await app.logIn(credentials);
+    const mongo = user.mongoClient("DATA_SOURCE_NAME");
+    const collection = mongo.db("DATABASE_NAME").collection("COLLECTION_NAME");
+
+    return Response.json(collection);
+
+}
 
 export const config = {runtime: "edge"};
