fb89d9ceeedf2019ffddb86d3fc52e2cf87ff42a
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-11-06 06:09:53 +0500

    realm-web

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-export const config = {runtime: "edge"};
+import * as Realm from "realm-web";
 
-export default () => Response.json({ok: true});
+const app = new Realm.App({id: "APP_ID"});
+const mongo = app.currentUser.mongoClient("DATA_SOURCE_NAME");
+const collection = mongo.db("DATABASE_NAME").collection("COLLECTION_NAME");
+
+export default () => Response.json(collection);
+
+export const config = {runtime: "edge"};
